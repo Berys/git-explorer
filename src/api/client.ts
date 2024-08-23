1fe6ad765c0c55531b4636f661956f0d4dbd1aaa
Borys Zięba <borys.zieba@geeknauts.com>
2024-08-23 19:46:05 +0200

    api config

---
+++ src/api/client.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import { GitHubSearchUsersResponse, UserRepositories } from './types/api-types';
+
+const GITHUB_API_URL = 'https://api.github.com';
+const GITHUB_TOKEN = 'your_github_token';
+
+export const axiosClient = axios.create({
+  baseURL: GITHUB_API_URL,
+  timeout: 10000,
+  headers: {
+    Authorization: `Bearer ${GITHUB_TOKEN}`,
+  },
+});
+
+export const searchGitHubUsers = async (
+  username: string,
+): Promise<GitHubSearchUsersResponse> => {
+  const response = await axios.get<GitHubSearchUsersResponse>(
+    `${GITHUB_API_URL}/search/users`,
+    {
+      params: {
+        q: username,
+        per_page: 5,
+      },
+    },
+  );
+  return response.data;
+};
+export const searchGitHubUserRepos = async (
+  username: string,
+): Promise<UserRepositories> => {
+  const response = await axios.get<UserRepositories>(
+    `${GITHUB_API_URL}/users/${username}/repos`,
+  );
+  return response.data;
+};
