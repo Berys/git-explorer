c63861608f975b7a594cacd5d1b797532fb56e9d
Borys Zięba <borys.zieba@geeknauts.com>
2024-08-23 20:10:42 +0200

    fetch github users

--- src/features/git-explorer/screens/git-explorer.tsx
+++ src/features/git-explorer/screens/git-explorer.tsx
@@ -3,10 +3,17 @@ import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { NativeStackScreenProps } from 'react-native-screens/lib/typescript/native-stack/types';
 import { AppStackParamList } from '@navigators/root-navigation';
-import { horizontalScale, normalize, verticalScale } from '@utils/theme-utils';
+import {
+  horizontalScale,
+  moderateScale,
+  normalize,
+  verticalScale,
+} from '@utils/theme-utils';
 import { colors } from '@theme/colors';
 import { typography } from '@theme/typography';
 import { SearchBar } from '@ui/searchbar';
+import { searchGitHubUsers } from '@api/client';
+import { useQuery } from '@tanstack/react-query';
 
 type GitExplorerProps = NativeStackScreenProps<
   AppStackParamList,
@@ -16,11 +23,21 @@ type GitExplorerProps = NativeStackScreenProps<
 export const GitExplorer = ({}: GitExplorerProps) => {
   const [searchQuery, setSearchQuery] = useState<string>('');
 
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['githubUser', searchQuery],
+    queryFn: () => searchGitHubUsers(searchQuery),
+    enabled: !!searchQuery,
+  });
+
+  const handleSearch = (query: string) => {
+    setSearchQuery(query);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.title}>Search</Text>
-        <SearchBar onSearch={() => {}} />
+        <SearchBar onSearch={handleSearch} />
         <Text style={styles.subtitle}>
           {searchQuery
             ? `Search results for "${searchQuery}"`
@@ -51,7 +68,7 @@ const styles = StyleSheet.create({
     fontSize: normalize(32),
   },
   spacer: {
-    borderBottomWidth: 2,
+    borderBottomWidth: moderateScale(2),
     marginVertical: verticalScale(20),
     borderColor: colors.palate.primary,
   },
