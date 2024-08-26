70d85a23ff5638ae9cc52147199feaae87c74f59
Borys Zięba <borys.zieba@geeknauts.com>
2024-08-26 07:30:18 +0200

    use axiosClient - fix

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,22 +3,18 @@ import { GitHubSearchUsersResponse, UserRepositories } from './types/api-types';
 import { delay } from '@utils/dev-utils';
 
 const GITHUB_API_URL = 'https://api.github.com';
-const GITHUB_TOKEN = 'your_github_token';
 
 export const axiosClient = axios.create({
   baseURL: GITHUB_API_URL,
   timeout: 10000,
-  headers: {
-    Authorization: `Bearer ${GITHUB_TOKEN}`,
-  },
 });
 
 export const searchGitHubUsers = async (
   username: string,
 ): Promise<GitHubSearchUsersResponse> => {
-  //For animation purposes
+  // For animation purposes
   await delay(2000);
-  const response = await axios.get<GitHubSearchUsersResponse>(
+  const response = await axiosClient.get<GitHubSearchUsersResponse>(
     `${GITHUB_API_URL}/search/users`,
     {
       params: {
@@ -32,9 +28,9 @@ export const searchGitHubUsers = async (
 export const searchGitHubUserRepos = async (
   username: string,
 ): Promise<UserRepositories> => {
-  //For animation purposes
+  // For animation purposes
   await delay(2000);
-  const response = await axios.get<UserRepositories>(
+  const response = await axiosClient.get<UserRepositories>(
     `${GITHUB_API_URL}/users/${username}/repos`,
   );
   return response.data;
